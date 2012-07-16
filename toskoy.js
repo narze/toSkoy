adcf1d772260ae3cd67b3d3afe7e02dfb232a999
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2012-07-16 21:50:12 +0700

    use a database for learning words. match by longest first

--- toskoy.js
+++ toskoy.js
@@ -1,5 +1,36 @@
 (function() {
-var skoyChar = {
+
+var db = {}
+  , maxLength = 0
+
+function learnWord(from, to) {
+  if (to.push && to.length) {
+    for (var i = 0; i < to.length; i ++) {
+      learnWord(from, to[i])
+    }
+    return
+  }
+  var array = db[from] || (db[from] = [])
+  array[array.length] = to
+  if (to.length > maxLength) maxLength = to.length
+}
+
+function learn(obj) {
+  for (var i in obj) {
+    learnWord(i, obj[i])
+  }
+}
+
+function has(str) {
+  return !!db[str]
+}
+
+function get(str) {
+  var c = db[str]
+  return c[~~(Math.random() * c)]
+}
+
+learn({
   'ู': "ุ๊",
   'ย': 'ญ',
   'ะ': '๊',
@@ -15,26 +46,35 @@ var skoyChar = {
   'ภ': 'พ',
   'า': '๊',
   'ฉ': 'ช๋'
-}
+})
 
-var skoyWord = {
+learn({
   'หนุ': 'นุ๊วซ์',
   'อยาก': 'ญั๊ข',
   'คะ': 'ขร๊',
   'ค่ะ': 'ฆร่',
   'ัน': 'ัล',
   'วก': '๊ก'
-}
+})
 
 String.prototype.toSkoy = function () {
   var skoy = ''
-  for (var i = 0 ; i < this.length ; i++) {
-    if (typeof skoyChar[this[i]] !== 'undefined') {
-      skoy = skoy + skoyChar[this[i]]
-    } else {
-      skoy = skoy + this[i]
+  for (var i = 0; i < this.length; ) {
+    var success = false
+    for (var l = maxLength; l > 0; l --) {
+      var sub = this.substr(i, l)
+      if (has(sub)) {
+        skoy += get(sub)
+        success = true
+        i += l
+        break
+      }
     }
+    if (success) continue
+    skoy += this.charAt(i)
+    i += 1
   }
   return skoy
 }
-})();
+
+})()
