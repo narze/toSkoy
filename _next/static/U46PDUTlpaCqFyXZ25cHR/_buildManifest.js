bc8f17e98943f989f80fee436f013e702387c7fe
narze <narze@users.noreply.github.com>
2020-09-21 03:28:19 +0000

    deploy: 9da76988b8014c21d061b4ee26b09644c020426b

---
+++ _next/static/U46PDUTlpaCqFyXZ25cHR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-e0f18f46da926156959e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f4f567df00ce0843964e.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
